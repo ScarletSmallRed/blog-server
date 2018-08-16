12e71b1ed5a46b4723e8d8016b97097c2d438ad3
沈韶泓 <shenshaohong@shenshaohongdeMacBook-Pro-2.local>
2018-08-16 11:59:08 +0800

    initialized express app

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
+
+app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+      error: {
+          message: error.message
+      }
+  });
+});
+
+app.listen(process.env.port || 4000, function(){
+  console.log('now listening for requests');
+});
